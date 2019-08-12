a04c168fc10694102f2f5abe8a64d5e91bd9682b
atn19808 <atn1908@outlook.com>
2019-08-12 11:51:29 -0500

    Updates

---
+++ precache-manifest.f7066cfe2c101688ddfcd0675758e035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c09a454a2c059d160126bdb8d7dc3b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "27b95ec18a6c9f1eec8b",
+    "url": "/monster-rolodex/static/css/main.f03c105b.chunk.css"
+  },
+  {
+    "revision": "f7e71d2cca931e5dd749",
+    "url": "/monster-rolodex/static/js/2.89e16e46.chunk.js"
+  },
+  {
+    "revision": "27b95ec18a6c9f1eec8b",
+    "url": "/monster-rolodex/static/js/main.03fc30a8.chunk.js"
+  },
+  {
+    "revision": "974ebdbc8482b2d59e77",
+    "url": "/monster-rolodex/static/js/runtime~main.cf73aa67.js"
+  }
+]);
